eb1d028011e2671bb636de651895c6b98f945c41
ij03l <idahorjoel@gmail.com>
2023-04-13 14:09:00 +0100

    project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,26 @@
 import React, { useEffect } from 'react'
+import o1 from "../assets/gallery/01.jpg";
+import o2 from "../assets/gallery/02.jpg";
+import o3 from "../assets/gallery/03.jpg";
+import o4 from "../assets/gallery/04.jpg";
+import o5 from "../assets/gallery/05.jpg";
+import o6 from "../assets/gallery/06.jpg";
+import o7 from "../assets/gallery/07.jpg";
+import o8 from "../assets/gallery/08.jpg";
+import o9 from "../assets/gallery/09.jpg";
+import o10 from "../assets/gallery/10.jpg";
+import o11 from "../assets/gallery/11.jpg";
+import o12 from "../assets/gallery/12.jpg";
+import o13 from "../assets/gallery/13.jpg";
+import o14 from "../assets/gallery/14.jpg";
+import o15 from "../assets/gallery/15.jpg";
+import o16 from "../assets/gallery/16.jpg";
+import o17 from "../assets/gallery/17.jpg";
+import o18 from "../assets/gallery/18.jpg";
+import o19 from "../assets/gallery/19.jpg";
+import o20 from "../assets/gallery/20.jpg";
+import o21 from "../assets/gallery/21.jpg";
+import o22 from "../assets/gallery/22.jpg";
 import image1 from "../assets/gallery/project-image -1.jpg";
 import image2 from "../assets/gallery/project-image -2.jpg";
 import image3 from "../assets/gallery/project-image -3.jpg";
@@ -49,6 +71,7 @@ import bs20 from "../assets/gallery/bussa20.jpg";
 import bs21 from "../assets/gallery/bussa21.jpg";
 import Aos from "aos";
 import "aos/dist/aos.css";
+import { Link } from "react-scroll";
 
 
 function Projects() {
@@ -124,79 +147,150 @@ function Projects() {
           </div>
         </div>
       </div>
-      <div className="w-full px-5 md:px-5">
-        <h1 className="md:text-4xl text-xl font-semibold capitalize">
-          Edo State Goverment{" "}
-          <span className="text-green-600 font-normal">Project</span>
-        </h1>
-        <hr className="my-5" />
-        <div className="w-full p-5 pb-10 mx-auto mb-10 gap-5 md:columns-3 columns-1  md:space-y-5">
-          <img src={image24} alt="project-image-1" data-aos="zoom-in" />
-          <img src={image5} alt="project-image-1" data-aos="zoom-in" />
-          <img src={image13} alt="project-image-1" data-aos="zoom-in" />
+      <div>
+        <div className="px-6">
+          <h1 className="text-6xl font-bold py-4">Welcome to our Gallery</h1>
+          {/* <p className="w-3/4 text-2xl">
+            As a team of creative professionals, we take pride in delivering
+            exceptional work that meets the unique needs of our clients. In this
+            gallery, we have curated a selection of some of our most outstanding
+            projects for you to explore. Each one showcases our dedication to
+            quality and attention to detail, and we are proud to have received
+            positive feedback from our satisfied clients. Whether you are
+            looking for inspiration or simply curious about our work, we invite
+            you to take a look and discover what sets us apart.
+          </p> */}
+          <div className="py-5 flex gap-3">
+            <Link
+              to="edo"
+              smooth={true}
+              duration={500}
+              className="btn btn-primary rounded-full"
+            >
+              Edo state Government
+            </Link>
+            <Link
+              to="bussa"
+              smooth={true}
+              duration={500}
+              className="btn btn-active rounded-full"
+            >
+              Bussa Projects
+            </Link>
+            <Link
+              to="others"
+              smooth={true}
+              duration={500}
+              className="btn btn-secondary rounded-full"
+            >
+              Other Projects
+            </Link>
+          </div>
         </div>
-      </div>
+        <div className="w-full px-5 md:px-5">
+          <h1 className="md:text-4xl text-xl font-semibold capitalize" id="edo">
+            Edo State Goverment{" "}
+            <span className="text-green-600 font-normal">Project</span>
+          </h1>
+          <hr className="my-5" />
+          <div className="w-full p-5 pb-10 mx-auto mb-10 gap-5 md:columns-3 columns-1  md:space-y-5">
+            {/* <img src={image24} alt="project-image-1" data-aos="zoom-in" /> */}
+            <img src={image5} alt="project-image-1" data-aos="zoom-in" />
+            <img src={image13} alt="project-image-1" data-aos="zoom-in" />
+          </div>
 
-      <div className="w-full px-5 md:px-5">
-        <h1 className="md:text-4xl text-xl font-semibold capitalize">
-          Bussa <span className="text-green-600 font-normal">Project</span>
-        </h1>
-        <hr className="my-5" />
-        <div className="w-full p-5 pb-10 mx-auto mb-10 gap-5 md:columns-3 columns-1  md:space-y-5">
-          <img src={image26} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs1} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs2} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs3} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs4} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs5} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs6} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs7} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs8} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs9} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs10} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs11} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs12} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs13} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs14} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs15} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs16} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs17} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs18} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs19} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs20} alt="Bussa project images" data-aos="zoom-in" />
-          <img src={bs21} alt="Bussa project images" data-aos="zoom-in" />
+          <div id="bussa" className="w-full px-5 md:px-5">
+            <h1
+              className="md:text-4xl text-xl font-semibold capitalize"
+             
+            >
+              Bussa <span className="text-green-600 font-normal">Project</span>
+            </h1>
+            <hr className="my-5" />
+            <div className="w-full p-5 pb-10 mx-auto mb-10 gap-5 md:columns-3 columns-1  md:space-y-5">
+              <img src={o1} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o2} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o3} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o4} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o5} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o6} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o7} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o8} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o9} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o10} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o11} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o12} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o13} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o14} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o15} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o16} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o17} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o18} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o19} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o20} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o21} alt="project-image-1" data-aos="zoom-in" />
+              <img src={o22} alt="project-image-1" data-aos="zoom-in" />
+            </div>
+          </div>
+
+          <h1
+            className="md:text-4xl text-xl font-semibold capitalize mx-5"
+            id="others"
+          >
+            Other <span className="text-green-600 font-normal">Project</span>
+          </h1>
+          <hr className="my-5" />
+
+          <div className="w-full p-5 pb-10 mx-auto mb-10 gap-5 md:columns-3 columns-1  md:space-y-5">
+            {/* <img src={image2} alt="project-image-1" data-aos="zoom-in" /> */}
+            <img src={image3} alt="project-image-1" data-aos="zoom-in" />
+            {/* <img src={image4} alt="project-image-1" data-aos="zoom-in" /> */}
+
+            <img src={image6} alt="project-image-1" data-aos="zoom-in" />
+            {/* <img src={image7} alt="project-image-1" data-aos="zoom-in" /> */}
+            {/* <img src={image8} alt="project-image-1" data-aos="zoom-in" /> */}
+            <img src={image9} alt="project-image-1" data-aos="zoom-in" />
+            {/* <img src={image10} alt="project-image-1" data-aos="zoom-in" /> */}
+            <img src={image11} alt="project-image-1" data-aos="zoom-in" />
+            {/* <img src={image12} alt="project-image-1" data-aos="zoom-in" /> */}
+            {/* <img src={image14} alt="project-image-1" data-aos="zoom-in" /> */}
+            <img src={image15} alt="project-image-1" data-aos="zoom-in" />
+            <img src={image16} alt="project-image-1" data-aos="zoom-in" />
+            <img src={image17} alt="project-image-1" data-aos="zoom-in" />
+            <img src={image18} alt="project-image-1" data-aos="zoom-in" />
+            <img src={image19} alt="project-image-1" data-aos="zoom-in" />
+            <img src={image20} alt="project-image-1" data-aos="zoom-in" />
+            <img src={image21} alt="project-image-1" data-aos="zoom-in" />
+            {/* <img src={image22} alt="project-image-1" data-aos="zoom-in" /> */}
+            {/* <img src={image23} alt="project-image-1" data-aos="zoom-in" /> */}
+            {/* <img src={image25} alt="project-image-1" data-aos="zoom-in" /> */}
+            {/* <img src={image27} alt="project-image-1" data-aos="zoom-in" /> */}
+          </div>
+          <div className="w-full p-5 pb-10 mx-auto mb-10 gap-5 md:columns-3 columns-1  md:space-y-5">
+            <img src={image26} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs1} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs2} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs3} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs4} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs5} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs6} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs7} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs8} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs9} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs10} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs11} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs12} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs13} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs14} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs15} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs16} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs17} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs18} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs19} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs20} alt="Bussa project images" data-aos="zoom-in" />
+            <img src={bs21} alt="Bussa project images" data-aos="zoom-in" />
+          </div>
         </div>
-      </div>
-
-      <h1 className="md:text-4xl text-xl font-semibold capitalize mx-5">
-        Other <span className="text-green-600 font-normal">Project</span>
-      </h1>
-      <hr className="my-5" />
-
-      <div className="w-full p-5 pb-10 mx-auto mb-10 gap-5 md:columns-3 columns-1  md:space-y-5">
-        <img src={image2} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image3} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image4} alt="project-image-1" data-aos="zoom-in" />
-
-        <img src={image6} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image7} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image8} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image9} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image10} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image11} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image12} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image14} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image15} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image16} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image17} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image18} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image19} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image20} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image21} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image22} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image23} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image25} alt="project-image-1" data-aos="zoom-in" />
-        <img src={image27} alt="project-image-1" data-aos="zoom-in" />
       </div>
     </div>
   );
